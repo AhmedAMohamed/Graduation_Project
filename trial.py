8b24b9df5b9689681f698c81a2eb54219b204a77
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2015-09-17 00:45:08 +0200

    first commit
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

---
+++ trial.py
@@ -0,0 +1,79 @@
+import wikipedia
+import requests
+
+'''
+ny = wikipedia.suggest("inter") # return string of a suggestion for search returns a string
+print(ny)
+'''
+'''
+ny = wikipedia.search('ahmed',5,True) # search in wiki for the word and returns a list limited to the number in the second param
+print(ny)
+'''
+'''
+ny = wikipedia.page('internet')  #
+print((ny))
+'''
+'''
+ny = wikipedia.random() # takes a param number of pages want to get randomly & returns pages titles
+print(ny)
+'''
+'''
+wikipedia.set_lang('ar') # set the language of the search and the returned pages
+ny = wikipedia.page('احمد')
+print(ny.content)
+'''
+'''
+wikipedia.set_rate_limiting() # set minimum waiting time for getting result from wiki
+'''
+'''
+ny = wikipedia.summary('internet', 0,150) # first param is the page title to summarize second if less than 10 and not equal 0 or less
+print(ny)
+'''
+title = 'Internet'
+page = wikipedia.page(title)
+content = page.content
+list_of_content = []
+list_of_sections = {}
+list_of_con_sum = {}
+html = page.html()
+cat = page.categories
+
+for item in content.split("\n"):
+    if "==" in item:
+        list_of_content.append(item.strip().replace('=','').strip())
+
+for it in list_of_content:
+    list_of_sections[it.split('Edit')[0]] = page.section(it)
+for it in list_of_sections:
+    if len(list_of_sections[it]) < 1:
+        continue
+    r = requests.post("http://autosummarizer.com/index.php", data={'text': list_of_sections[it]})
+    text = r.text
+    index_first , index_second = text.find('<div id="position">') , text.find('<br><br></b></div>')
+    text = text[index_first:index_second]
+    final_text = ""
+
+    for line in text.split('<br><br>'):
+        line = line.replace('<div id="position"><b>',' ')
+        line.strip()
+        final_text = final_text + '\n' + line
+
+    list_of_con_sum[it] = final_text
+
+for key in list_of_con_sum:
+    print(key)
+    print(list_of_con_sum[key][3:])
+    print()
+
+print()
+print()
+#for key in list_of_sections:
+#    if len(list_of_sections[key]) < 1:
+    #    print(key)
+
+
+#r = requests.post("http://autosummarizer.com/index.php", data={'text': list_of_sections['Licensing of repositories']})
+#text = r.text
+#index_first , index_second = text.find('<div id="position">') , text.find('<br><br></b></div>')
+#text = text[index_first:index_second]
+#print(text)
