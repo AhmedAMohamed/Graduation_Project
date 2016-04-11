6c682ece362741985c0b2c44c89252898e137870
FatmaGamal <hermonie.23@gmail.com>
2016-04-12 00:53:06 +0200

    getting root of single mindmap

--- Analysis/Pre_processing/Java/src/grad/project/srl/DMRGraph.java
+++ Analysis/Pre_processing/Java/src/grad/project/srl/DMRGraph.java
@@ -1,57 +1,73 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-
 public class DMRGraph {
-private Map<String,Argument> ArgsHash;
-private ArrayList<Frame>ActionFrames;
+	private Map<String, Integer> scoreRoot;
+	private Map<String, Argument> ArgsHash;
+	private ArrayList<Frame> ActionFrames;
 
-DMRGraph(ArrayList<Frame>frames){
-	ArgsHash=new HashMap<String,Argument>();
-	ActionFrames=frames;
-	
-}
-public void createGraph(){
-	
-	Iterator<Frame>ActionFramesIterator=ActionFrames.iterator(); 
-	while(ActionFramesIterator.hasNext()){
-		//Iterate over all the pairs in the hashmap of ActionArgs
-		Frame ActionFrame=ActionFramesIterator.next();
-		for(Map.Entry<String, ArrayList<Argument>> ArgPair :ActionFrame.arguments.entrySet()){
-			ArrayList<Argument>FrameArguments=ArgPair.getValue();
-			
-			for(Argument Arg:FrameArguments){
-				String Word;
-				//Check if the Argument has a coref
-				/*if(Arg.Coref!=null){
-					 Word=Arg.Coref;			
-				}
-				else {
-					
-					Word=Arg.word;
-				}*/
-				Word=Arg.word;
+	DMRGraph(ArrayList<Frame> frames) {
+		ArgsHash = new HashMap<String, Argument>();
+		ActionFrames = frames;
 
-				if(ArgsHash.get(Word)!=null)
-				{
-				Arg= ArgsHash.get(Word);	
+	}
+
+	public ArrayList<String> createGraph() {
+
+		Iterator<Frame> ActionFramesIterator = ActionFrames.iterator();
+		while (ActionFramesIterator.hasNext()) {
+			// Iterate over all the pairs in the hashmap of ActionArgs
+			Frame ActionFrame = ActionFramesIterator.next();
+			for (Map.Entry<String, ArrayList<Argument>> ArgPair : ActionFrame.arguments.entrySet()) {
+				ArrayList<Argument> FrameArguments = ArgPair.getValue();
+
+				for (Argument Arg : FrameArguments) {
+					String Word;
+					// Check if the Argument has a coref
+					/*
+					 * if(Arg.Coref!=null){ Word=Arg.Coref; } else {
+					 * 
+					 * Word=Arg.word; }
+					 */
+					Word = Arg.word;
+
+					if (ArgsHash.get(Word) != null) {
+						Arg = ArgsHash.get(Word);
+
+					} else {
+						ArgsHash.put(Word, Arg);
+
+					}
 					
-				}
-				else {
-				ArgsHash.put(Word, Arg);
+					ArgsHash.get(Word).argumentType = null;
 					
+					if (scoreRoot.containsKey(Word)) {
+						scoreRoot.put(Word, 1);
+					} else {
+						scoreRoot.put(Word, scoreRoot.get(Word) + 1);
+					}
+
 				}
 				
 			}
-			
-			
-			
 		}
 		
+
+		ArrayList<String> root = new ArrayList<String>();
+		int maxScore = Collections.max(scoreRoot.values());
+		Iterator<Entry<String, Integer>> rootIterator = scoreRoot.entrySet().iterator();
+		while (rootIterator.hasNext()) {
+			Entry<String, Integer> rootPair = rootIterator.next();
+			if (rootPair.getValue() == maxScore) {
+				root.add(rootPair.getKey());
+			}
+		}
+		return root;
+		
 	}
-	
-}
+
 }
